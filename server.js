1ce10cb433e2605d1ae6b3388defbaa1992b5812
ag-jod00 <abhinandan02102010@gmail.com>
2025-02-21 21:52:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,55 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-const rooms = {}; // Stores active rooms and players
+const rooms = {}; // Store active rooms
 
-app.use(express.static(__dirname)); // Serve static files from project folder
-
-// Serve the main room creation page on root
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index05.html"));
+// Set `index01.html` as the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index01.html'));
 });
 
-io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+// Serve other static files
+app.use(express.static(__dirname));
 
-    socket.on("createRoom", () => {
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('createRoom', () => {
         const roomCode = Math.floor(100000 + Math.random() * 900000).toString();
-        rooms[roomCode] = { players: [socket.id], board: ["", "", "", "", "", "", "", "", ""] };
-
+        rooms[roomCode] = { players: [socket.id] };
         socket.join(roomCode);
-        socket.emit("roomCreated", roomCode);
-        console.log(`Room ${roomCode} created by ${socket.id}`);
+        socket.emit('roomCreated', roomCode);
+        console.log(`Room created: ${roomCode}`);
     });
 
-    socket.on("joinRoom", (roomCode) => {
+    socket.on('joinRoom', (roomCode) => {
         if (rooms[roomCode] && rooms[roomCode].players.length < 2) {
             rooms[roomCode].players.push(socket.id);
             socket.join(roomCode);
-            socket.emit("roomJoined", roomCode);
-            io.to(roomCode).emit("startGame", rooms[roomCode].players);
-            console.log(`Player ${socket.id} joined room ${roomCode}`);
+            io.to(roomCode).emit('roomJoined', roomCode);
+            console.log(`Player joined room: ${roomCode}`);
         } else {
-            socket.emit("roomError", "Room is full or does not exist!");
+            socket.emit('roomError', 'Room is full or does not exist!');
         }
     });
 
-    socket.on("makeMove", ({ roomCode, index, symbol }) => {
-        if (rooms[roomCode]) {
-            rooms[roomCode].board[index] = symbol;
-            io.to(roomCode).emit("updateBoard", rooms[roomCode].board);
-        }
-    });
-
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
         for (const roomCode in rooms) {
-            const players = rooms[roomCode].players;
-            if (players.includes(socket.id)) {
-                rooms[roomCode].players = players.filter((id) => id !== socket.id);
-                if (rooms[roomCode].players.length === 0) {
-                    delete rooms[roomCode];
-                    console.log(`Room ${roomCode} deleted`);
-                }
+            rooms[roomCode].players = rooms[roomCode].players.filter(id => id !== socket.id);
+            if (rooms[roomCode].players.length === 0) {
+                delete rooms[roomCode];
             }
         }
-        console.log("User disconnected:", socket.id);
+        console.log('A user disconnected:', socket.id);
     });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
